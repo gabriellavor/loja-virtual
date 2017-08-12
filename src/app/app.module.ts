7a8b0b84e92df81992e9d1548c9bd622f790f965
gabriellavor <gvlavor@gmail.com>
2017-08-12 12:45:37 -0300

    Primeiro Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CategoriaComponent } from './categoria/categoria.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { BoxComponent } from './box/box.component';
+import { TopoComponent } from './topo/topo.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoriaComponent,
+    ToolbarComponent,
+    BoxComponent,
+    TopoComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule
